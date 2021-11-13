8f4c6805ae3fee8df521236b9958ff2a55bc28b7
debanjandhar12 <debanjandhar12@gmail.com>
2021-11-13 10:46:34 +0530

    Support center and comment org blocks

--- index.ts
+++ index.ts
@@ -155,6 +155,12 @@ async function addClozesToMdAndConvertToHtml(text: string, regexArr: any): Promi
   res = res.replace(/\$\$([\s\S]*?)\$\$/g, "\\[ $1 \\]"); // Convert block math
   res = res.replace(/#\+BEGIN_(QUOTE)( .*)?\n((.|\n)*?)#\+END_\1/gi, function(match, g1, g2, g3) { // Convert quote org blocks
     return `<blockquote">${g3.trim()}</blockquote>`;
+  });
+  res = res.replace(/#\+BEGIN_(CENTER)( .*)?\n((.|\n)*?)#\+END_\1/gi, function(match, g1, g2, g3) { // Convert center org blocks
+    return `<span class="text-center">${g3.trim()}</span>`; // div is buggy with remarkable
+  });
+  res = res.replace(/#\+BEGIN_(COMMENT)( .*)?\n((.|\n)*?)#\+END_\1/gi, function(match, g1, g2, g3) { // Convert comment org blocks
+    return ``; 
   }); 
   res = res.replace(/#\+BEGIN_(\w+)( .*)?\n((.|\n)*?)#\+END_\1/gi, function(match, g1, g2, g3) { // Convert named org blocks
     return `<span class="${g1.toLowerCase()}">${g3.trim()}</span>`; // div is buggy with remarkable
