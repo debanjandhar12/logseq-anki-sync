6281912c5ea202cc841d3435ac87d629ea5f63ec
debanjandhar12 <debanjandhar12@gmail.com>
2021-12-22 22:33:04 +0530

    fix: prevent error if query returns old result

--- src/MultilineCardBlock.ts
+++ src/MultilineCardBlock.ts
@@ -31,23 +31,23 @@ export class MultilineCardBlock extends Block {
     public addClozes(): MultilineCardBlock {
         let result = this.content;
         let direction = _.get(this, 'properties.direction');
-        if(direction != "->" && direction != "<-" && direction != "<->") {
-            if((this.tags.includes("reversed") && this.tags.includes("forward")) || this.tags.includes("bidirectional")) direction = "<->";
-            else if(this.tags.includes("reversed")) direction = "<-";
+        if (direction != "->" && direction != "<-" && direction != "<->") {
+            if ((this.tags.includes("reversed") && this.tags.includes("forward")) || this.tags.includes("bidirectional")) direction = "<->";
+            else if (this.tags.includes("reversed")) direction = "<-";
             else direction = "->";
-        } 
+        }
 
         // Add cloze to the parent block if direction is <-> or <-
         result = result.replace(/(\{\{c(\d+)::)((.|\n)*)\}\}/g, "$3");
-        if(direction == "<->" || direction == "<-") 
+        if (direction == "<->" || direction == "<-")
             result = `{{c2:: \n ${result} \n}}`;
-        
+
         // Add the content of children blocks and cloze it if direction is <-> or ->
-        result+=`\n<ul class="children-list left-border">`;
-        for(const child of this.children) {
+        result += `\n<ul class="children-list left-border">`;
+        for (const child of this.children) {
             result += `\n<li class="children">`;
             let sanitized_html_content = child.html_content.replace(/(\{\{c(\d+)::)((.|\n)*)\}\}/g, "$3");
-            if(direction == "<->" || direction == "->")
+            if (direction == "<->" || direction == "->")
                 result += `{{c1:: ${sanitized_html_content} }}`;
             else result += `${sanitized_html_content}`;
             result += `</li>`;
@@ -76,13 +76,16 @@ export class MultilineCardBlock extends Block {
         blocks = await Promise.all(blocks.map(async (block) => {
             let uuid = block[0].uuid["$uuid$"] || block[0].uuid.Wd;
             let page = (block[0].page) ? await logseq.Editor.getPage(block[0].page.id) : {};
-            block = await logseq.Editor.getBlock(uuid,{includeChildren: true});
-            let tags = await Promise.all(_.map(block.refs, async page => { return _.get(await logseq.Editor.getPage(page.id), 'name') }));
-            console.log(tags);
-            let children = await Promise.all(_.map(block.children, async child => _.extend({html_content: await Converter.convertToHtml(child.content)}, child))) || [];
-            return new MultilineCardBlock(uuid, block.content, block.format, block.properties || {}, page, tags, children);
+            block = await logseq.Editor.getBlock(uuid, { includeChildren: true });
+            if (block) {
+                let tags = await Promise.all(_.map(block.refs, async page => { return _.get(await logseq.Editor.getPage(page.id), 'name') }));
+                console.log(tags);
+                let children = await Promise.all(_.map(block.children, async child => _.extend({ html_content: await Converter.convertToHtml(child.content) }, child))) || [];
+                return new MultilineCardBlock(uuid, block.content, block.format, block.properties || {}, page, tags, children);
+            } else return null;
         }));
         blocks = _.uniqBy(blocks, 'uuid');
+        blocks = _.without(blocks, undefined, null);
 
         return blocks;
     }
