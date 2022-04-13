5d5fff325db1511046cb02f2b0707ec92b5350d1
debanjandhar12 <debanjandhar12@gmail.com>
2022-04-13 14:16:18 +0530

    feat: add scrollToClozeElement toolbar button
    adds scrollToClozeElement toolbar button to anki card display

--- src/templates/logseq_anki_sync.js
+++ src/templates/logseq_anki_sync.js
@@ -1,3 +1,9 @@
 /***
 * This files contains the js for the both sides of anki cards. 
 */
+
+function scrollToClozeElement() {
+    let element = document.getElementsByClassName("cloze")[0];
+    element.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+    console.log("Scrolled to cloze element");
+}
