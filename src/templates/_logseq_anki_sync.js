1d87fd12bce0693a377cfaa02264822014717d57
debanjandhar12 <debanjandhar12@gmail.com>
2022-05-21 14:46:28 +0530

    feat: add backlink to edit the anki card in logseq #69

--- src/templates/_logseq_anki_sync.js
+++ src/templates/_logseq_anki_sync.js
@@ -7,3 +7,11 @@ function scrollToClozeElement() {
     element.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
     console.log("Scrolled to cloze element");
 }
+
+function openBlockInLogseq(uuid) {
+    if(uuid == null || uuid == "") return;
+    let element = document.getElementsByClassName("breadcrumb2")[0];
+    // javascript get first child link element
+    let link = element.getElementsByTagName("a")[0];
+    window.open(`${link.href.match(/logseq:\/\/graph\/.*\?/)}block-id=${uuid}`, '_blank')
+}
