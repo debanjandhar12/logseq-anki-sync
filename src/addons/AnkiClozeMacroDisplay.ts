007be0f2a7db95a23d926201c6219a72a0aea84c
debanjandhar12 <debanjandhar12@gmail.com>
2022-07-19 20:26:38 +0530

    enhance: anki cloze marco display addon

--- src/addons/AnkiClozeMacroDisplay.ts
+++ src/addons/AnkiClozeMacroDisplay.ts
@@ -4,8 +4,6 @@ import { Addon } from "./Addon";
 export class AnkiClozeMacroDisplay extends Addon {
     static _instance: AnkiClozeMacroDisplay;
     static observer;
-    static lastObserved;
-    static timeOutFunc;
     static hooks = [];
 
     public getName(): string {
@@ -15,34 +13,29 @@ export class AnkiClozeMacroDisplay extends Addon {
     public init(): void {
         if (!this.isEnabled()) return;
         // Set up observer for Anki Cloze Macro Syntax
-        const watchTarget = window.parent.document.getElementById("app-container");
-        AnkiClozeMacroDisplay.timeOutFunc = null;
-        AnkiClozeMacroDisplay.lastObserved = Date.now() - 1000;
         let displayAnkiCloze = () => {
             // console.log("Display");
             if (!this.isEnabled())  return;
-            let clozes = window.parent.document.querySelectorAll(
+            let clozes : Element | NodeListOf<Element> = window.parent.document.getElementById("app-container").querySelector(
+                'span[title^="Unsupported macro name: c"]'
+            );
+            if (!clozes) return;
+            clozes = window.parent.document.getElementById("app-container").querySelectorAll(
                 'span[title^="Unsupported macro name: c"]'
             );
             clozes.forEach((cloze) => {
                 if (/c\d$/.test((cloze as Element & {title}).title))
-                    cloze.outerHTML = `<span style="background-color:rgb(59 130 246 / 0.1);">${cloze.innerHTML.replace(/^{{{c\d (.*)}}}$/,"$1")}</span>`;
+                    cloze.outerHTML = `<span style="background-color:rgb(59 130 246 / 0.1);">${cloze.innerHTML.replace(/^{{{c\d (.*?)(::.*)}}}$/,"$1")}</span>`;
             });
-            AnkiClozeMacroDisplay.lastObserved = Date.now();
         }
         AnkiClozeMacroDisplay.observer = new MutationObserver((mutations) => {
-            if(Date.now() - AnkiClozeMacroDisplay.lastObserved < 1000 && mutations.length < 50) {
-                clearTimeout(AnkiClozeMacroDisplay.timeOutFunc);
-                AnkiClozeMacroDisplay.timeOutFunc = setTimeout(displayAnkiCloze, 1000 - (Date.now() - AnkiClozeMacroDisplay.lastObserved));
-                return;
-            }
             displayAnkiCloze();
-            clearTimeout(AnkiClozeMacroDisplay.timeOutFunc);
         });
-        AnkiClozeMacroDisplay.hooks.push(logseq.App.onCurrentGraphChanged(() => { displayAnkiCloze(); }));
-        AnkiClozeMacroDisplay.hooks.push(logseq.Editor.onBlockChanged(() => { displayAnkiCloze(); }));
-        AnkiClozeMacroDisplay.hooks.push(logseq.Editor.onSidebarVisibleChanged(() => { displayAnkiCloze(); }));
-        AnkiClozeMacroDisplay.observer.observe(watchTarget, {
+        AnkiClozeMacroDisplay.observer.observe(window.parent.document.getElementById("main-content-container"), {
+            subtree: true,
+            childList: true
+        });
+        AnkiClozeMacroDisplay.observer.observe(window.parent.document.getElementById("right-sidebar"), {
             subtree: true,
             childList: true
         });
