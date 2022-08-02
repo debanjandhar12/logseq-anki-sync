8d84765a6de5eba1e3e6b18b2eb9a8e9e97f766c
debanjandhar12 <debanjandhar12@gmail.com>
2022-08-02 18:05:48 +0530

    fix: several issues with swift note

--- src/notes/SwiftNote.ts
+++ src/notes/SwiftNote.ts
@@ -3,7 +3,8 @@ import '@logseq/libs'
 import { safeReplace } from '../utils';
 import _ from 'lodash';
 import { MD_PROPERTIES_REGEXP, ORG_PROPERTIES_REGEXP } from "../constants";
-import { SyncronizedLogseq } from "../SyncronizedLogseq";
+import { LogseqProxy } from "../LogseqProxy";
+import { convertToHTMLFile, HTMLFile } from "../converter/Converter";
 
 export class SwiftNote extends Note {
     public type: string = "swift";
@@ -14,32 +15,30 @@ export class SwiftNote extends Note {
 
     public static initLogseqOperations = (() => { });
 
-    public addClozes(): SwiftNote {
-        let result : string = this.content;
+    public async getClozedContentHTML(): Promise<HTMLFile> {
+        let clozedContent : string = this.content;
 
         // Remove logseq properties as it might cause problems during cloze creation
-        result = safeReplace(result, MD_PROPERTIES_REGEXP, ""); //Remove md properties
-        result = safeReplace(result, ORG_PROPERTIES_REGEXP, ""); //Remove org properties
-    
+        clozedContent = safeReplace(clozedContent, MD_PROPERTIES_REGEXP, ""); //Remove md properties
+        clozedContent = safeReplace(clozedContent, ORG_PROPERTIES_REGEXP, ""); //Remove org properties
         // --- Add clozes ---
-        result = safeReplace(result, /(.*?)(\s*(:<->|:->|:<-)\s*)(.+)/s, (match, g1, g2, g3, g4) => {
+        clozedContent = safeReplace(clozedContent, /(.*?)(\s*(:<->|:->|:<-)\s*)(.+)/s, (match, g1, g2, g3, g4) => {
             let replacement = "";
             if(g3 == ":<-" || g3 == ":<->") {
-                replacement += `{{c2::${g1.trim()}}}`;
+                replacement += `{{c2::${g1.replace(/(?<!{{embed [^}\n]*?)}}/g, "}\u{2063}}").trim()}}}`;
             }
             else replacement += `${g1.trim()}`;
             let beforeArrowSpace = g2.split(/(:<->|:->|:<-)/s)[0];
             let afterArrowSpace = g2.split(/(:<->|:->|:<-)/s)[2];
             replacement += `${beforeArrowSpace}<b>${g3}</b>${afterArrowSpace}`;
             if(g3 == ":->" || g3 == ":<->") {
-                replacement += `{{c1::${g4.trim()}}}`;
+                replacement += `{{c1::${g4.replace(/(?<!{{embed [^}\n]*?)}}/g, "}\u{2063}}").trim()}}}`;
             }
             else replacement += `${g4.trim()}`;
             return replacement;
         });
 
-        this.content = result;
-        return this;
+        return convertToHTMLFile(clozedContent, this.format);
     }
 
     public static async getNotesFromLogseqBlocks(): Promise<SwiftNote[]> {
@@ -53,10 +52,10 @@ export class SwiftNote extends Note {
         let blocks: any = [...singleSwiftBlocks];
         blocks = await Promise.all(blocks.map(async (block) => {
             let uuid = block[0].uuid["$uuid$"] || block[0].uuid.Wd;
-            let page = (block[0].page) ? await SyncronizedLogseq.Editor.getPage(block[0].page.id) : {};
+            let page = (block[0].page) ? await LogseqProxy.Editor.getPage(block[0].page.id) : {};
             block = block[0];
             if(!block.content) {
-                block = await SyncronizedLogseq.Editor.getBlock(uuid);
+                block = await LogseqProxy.Editor.getBlock(uuid);
             }
             if(block)
                 return new SwiftNote(uuid, block.content, block.format, block.properties || {}, page);
@@ -69,6 +68,15 @@ export class SwiftNote extends Note {
         blocks = _.without(blocks, undefined, null);
         blocks = _.filter(blocks, (block) => { // Remove template cards
             return _.get(block, 'properties.template') == null || _.get(block, 'properties.template') == undefined;
+        });
+        blocks = _.filter(blocks, (block) => { // Remove notes that do not genetate any clozes
+            let block_content = block.content;
+            let cardGenerated = false;
+            safeReplace(block_content, /(.*?)(\s*(:<->|:->|:<-)\s*)(.+)/s, (match, g1, g2, g3, g4) => {
+                cardGenerated = true;
+                return match;
+            });
+            return cardGenerated;
         });
         return blocks;
     }
