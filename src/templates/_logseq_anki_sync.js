a11259a03e8887b44484a1f6c41a88e8f88a5082
debanjandhar12 <debanjandhar12@gmail.com>
2022-10-11 19:58:57 +0530

    change prod build to parcel watch
    Prod build causes:
    - Imports in _logseq_anki_sync.js to breaks
    -Mldocs to break in Parcel 2.6.0 and above

--- src/templates/_logseq_anki_sync.js
+++ src/templates/_logseq_anki_sync.js
@@ -2,20 +2,18 @@
 * This files contains the js for the both sides of anki cards. 
 */
 
-window.onload = function () {
-    function scrollToClozeElement() {
-        let element = document.getElementsByClassName("cloze")[0];
-        element.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
-        console.log("Scrolled to cloze element");
-    }
+window.scrollToClozeElement = () => {
+    let element = document.getElementsByClassName("cloze")[0];
+    element.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+    console.log("Scrolled to cloze element");
+}
 
-    function openBlockInLogseq(uuid) {
-        if(uuid == null || uuid == "") return;
-        let element = document.getElementsByClassName("breadcrumb2")[0];
-        // javascript get first child link element
-        let page_link = element.getElementsByTagName("a")[0];
-        let block_link = document.createElement("a");
-        block_link.href = `${page_link.href.match(/logseq:\/\/graph\/.*\?/)}block-id=${uuid}`;
-        block_link.click();
-    }
+window.openBlockInLogseq = (uuid) => {
+    if(uuid == null || uuid == "") return;
+    let element = document.getElementsByClassName("breadcrumb2")[0];
+    // javascript get first child link element
+    let page_link = element.getElementsByTagName("a")[0];
+    let block_link = document.createElement("a");
+    block_link.href = `${page_link.href.match(/logseq:\/\/graph\/.*\?/)}block-id=${uuid}`;
+    block_link.click();
 }
