0c1ddfbcb4028de335d31c207a43c4caa73c50e9
debanjandhar12 <49021233+debanjandhar12@users.noreply.github.com>
2023-01-28 10:53:54 +0530

    feat: consider direct ref images in image occlusion

--- src/notes/ImageOcclusionNote.ts
+++ src/notes/ImageOcclusionNote.ts
@@ -1,8 +1,19 @@
 import { Note } from "./Note";
 import '@logseq/libs'
-import { escapeClozeAndSecoundBrace, safeReplace } from '../utils/utils';
+import {
+    escapeClozeAndSecoundBrace,
+    getFirstNonEmptyLine,
+    getRandomUnicodeString,
+    safeReplace,
+    safeReplaceAsync
+} from '../utils/utils';
 import _ from 'lodash';
-import {MD_IMAGE_EMBEDED_REGEXP, MD_PROPERTIES_REGEXP, ORG_PROPERTIES_REGEXP} from "../constants";
+import {
+    LOGSEQ_BLOCK_REF_REGEXP,
+    MD_IMAGE_EMBEDED_REGEXP,
+    MD_PROPERTIES_REGEXP,
+    ORG_PROPERTIES_REGEXP
+} from "../constants";
 import { LogseqProxy } from "../logseq/LogseqProxy";
 import {convertToHTMLFile, HTMLFile, processProperties} from "../converter/Converter";
 import {SelectPrompt} from "../ui/SelectPrompt";
@@ -21,11 +32,7 @@ export class ImageOcclusionNote extends Note {
         logseq.Editor.registerBlockContextMenuItem("Image Occlusion", async (block) => {
             let uuid = getUUIDFromBlock(block as BlockEntity);
             block = await logseq.Editor.getBlock(uuid); // Dont use LogseqProxy.Editor.getBlock() here. It will cause a bug due to activeCache.
-            let block_content = block.content; // Process pdf properties
-            let block_images = ((await processProperties(block_content)).match(MD_IMAGE_EMBEDED_REGEXP) || []).map((block_image) => {
-                return block_image.replace(MD_IMAGE_EMBEDED_REGEXP, "$1");
-            });
-            block_images = _.uniq(block_images);
+            let block_images = await ImageOcclusionNote.getImagesInBlock(block);
             if (block_images.length == 0) {await logseq.UI.showMsg("No images found in this block.", "warning"); return;}
             let imgToOcclusionArrHashMap = JSON.parse(Buffer.from(block.properties?.occlusion || Buffer.from("{}", 'utf8').toString('base64'), 'base64').toString());
             console.log("imgToOcclusionArrHashMap", imgToOcclusionArrHashMap);
@@ -49,9 +56,8 @@ export class ImageOcclusionNote extends Note {
         let clozes = new Set();
         for(let image in imgToOcclusionArrHashMap) {
             let occlusionArr = imgToOcclusionArrHashMap[image];
-            let block_images = (await processProperties(this.content)).match(MD_IMAGE_EMBEDED_REGEXP).map((image) => {
-                return image.replace(MD_IMAGE_EMBEDED_REGEXP, "$1");
-            });
+            let block = await LogseqProxy.Editor.getBlock(this.uuid);
+            let block_images = await ImageOcclusionNote.getImagesInBlock(block);
             if(block_images.includes(image)) {
                 for (let occlusion of occlusionArr) {
                     clozes.add(occlusion.cId);
@@ -98,9 +104,7 @@ export class ImageOcclusionNote extends Note {
                 let imgToOcclusionArrHashMap = JSON.parse(Buffer.from(block.properties?.occlusion, 'base64').toString());
                 for(let image in imgToOcclusionArrHashMap) {
                     let occlusionArr = imgToOcclusionArrHashMap[image];
-                    let blockImages = (await processProperties(block.content)).match(MD_IMAGE_EMBEDED_REGEXP).map((image) => {
-                        return image.replace(MD_IMAGE_EMBEDED_REGEXP, "$1");
-                    });
+                    let blockImages = await ImageOcclusionNote.getImagesInBlock(block);
                     if(occlusionArr && occlusionArr.length > 0 && blockImages.includes(image))
                         return block;    // Found a valid occlusion! Return true.
                 }
@@ -111,5 +115,32 @@ export class ImageOcclusionNote extends Note {
         blocks = _.without(blocks, false);
         // console.log(blocks);
         return blocks;
+    }
+
+    // -- Helper functions --
+    public static async getImagesInBlock(block: any): Promise<string[]> {
+        let block_content = block.content;
+        block_content = await safeReplaceAsync(block_content, LOGSEQ_BLOCK_REF_REGEXP, async (match, blockUUID) => { // Add contents of direct block refs (1-level)
+            try {
+                let block_ref = await logseq.Editor.getBlock(blockUUID); // Dont use LogseqProxy.Editor.getBlock() here. It will cause a bug due to activeCache.
+                let block_ref_content = _.get(block_ref, "content");
+                let block_ref_props = _.get(block_ref, "properties");
+                block_content = safeReplace(block_ref_content, MD_PROPERTIES_REGEXP, "");
+                block_content = safeReplace(block_ref_content, ORG_PROPERTIES_REGEXP, "");
+                let block_ref_content_first_line = getFirstNonEmptyLine(block_ref_content).trim();
+                let block_ref_props_str = block_ref_props ? Object.keys(block_ref_props).map((key) => {
+                    return `${key}::${block_ref_props[key]}`;
+                }).join('\n') : "";
+                return block_ref_props_str + "\n" + block_ref_content_first_line;
+            }
+            catch (e) { console.warn(e); }
+            return match;
+        });
+        block_content = await processProperties(block_content); // Process pdf properties
+        let block_images = (block_content.match(MD_IMAGE_EMBEDED_REGEXP) || []).map((block_image) => {
+            return block_image.replace(MD_IMAGE_EMBEDED_REGEXP, "$1");
+        });
+        block_images = _.uniq(block_images);
+        return block_images;
     }
 }
