65482e3f6d2d21c0d020a2acbbd4c80ec4777fe5
debanjandhar12 <debanjandhar12@gmail.com>
2024-02-09 23:52:55 +0530

    chore: update version

--- src/index.ts
+++ src/index.ts
@@ -89,9 +89,10 @@ function main(baseInfo: LSPluginBaseInfo) {
         showModelWithButtons(
             `<span class="flex items-center"><i class="px-1">${ANKI_ICON}</i>Welcome to Logseq Anki Sync ${baseInfo.version}!</span> 
                                     <br/><small class="px-2">Update is installed successfully. </small>
-                                    <br /><br /><small class="px-2">In this version, automated testing was added to prevent regression, and several bugs were squashed.</small>
-                                    <br/><br/><div class="flex flex-row align-items"><div title="Warning" class="pr-4 admonition-icon flex flex-col justify-center"><svg viewBox="0 0 576 512" fill="var(--ls-warning-color)" class="h-8 w-8 warning"><path d="M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"></path></svg></div><div class="ml-4">
-                                    <small>This plugin is now incompatible with Logseq versions earlier than <b style="color: var(--ls-wb-background-color-red)">0.10.5</b>. Sadly this had to be done as supporting older versions was consuming significant development time.</small></div></div></div></div>
+                                    <br /><br /><small class="px-2" style="display: block">In this version, <code>#hide-all-test-one</code> tag is introduced for multiline incremental cards and clozes. Serveral bugs were also fixed. The feature vote survey is also brought back for feedback purposes.</small>
+                                    <br/><div class="flex flex-row align-items"></div><div class="ml-4">
+                                    <div class="flex flex-row admonitionblock align-items tip"><div title="Tip" class="pr-4 admonition-icon flex flex-col justify-center"><svg viewBox="0 0 352 512" fill="currentColor" class="h-8 w-8 tip"><path d="M96.06 454.35c.01 6.29 1.87 12.45 5.36 17.69l17.09 25.69a31.99 31.99 0 0 0 26.64 14.28h61.71a31.99 31.99 0 0 0 26.64-14.28l17.09-25.69a31.989 31.989 0 0 0 5.36-17.69l.04-38.35H96.01l.05 38.35zM0 176c0 44.37 16.45 84.85 43.56 115.78 16.52 18.85 42.36 58.23 52.21 91.45.04.26.07.52.11.78h160.24c.04-.26.07-.51.11-.78 9.85-33.22 35.69-72.6 52.21-91.45C335.55 260.85 352 220.37 352 176 352 78.61 272.91-.3 175.45 0 73.44.31 0 82.97 0 176zm176-80c-44.11 0-80 35.89-80 80 0 8.84-7.16 16-16 16s-16-7.16-16-16c0-61.76 50.24-112 112-112 8.84 0 16 7.16 16 16s-7.16 16-16 16z"></path></svg></div><div class="ml-4 text-sm"><p>
+                                    <small><b>Vote Features for next version: <a href="https://forms.gle/DCQ6vLwBHpzjSegQ9">https://forms.gle/DCQ6vLwBHpzjSegQ9</a></b><br/>The link will be available at the bottom of plugin README page as well. </p></div></div></small></div></div></div></div>
                                     `,
             [
                 {
@@ -99,6 +100,15 @@ function main(baseInfo: LSPluginBaseInfo) {
                     f: () => {
                         window.open(
                             `https://github.com/debanjandhar12/logseq-anki-sync/releases/tag/v${pkg.version}`,
+                        );
+                    },
+                    returnOnClick: false,
+                },
+                {
+                    name: "Vote Features",
+                    f: () => {
+                        window.open(
+                            `https://forms.gle/DCQ6vLwBHpzjSegQ9`,
                         );
                     },
                     returnOnClick: false,
