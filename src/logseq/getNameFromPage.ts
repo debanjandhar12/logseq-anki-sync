c104d1d627618d9b63c28e49a66f5e9c278a53b5
debanjandhar12 <debanjandhar12@gmail.com>
2026-01-17 18:17:46 +0530

    fix get name

--- src/logseq/getNameFromPage.ts
+++ src/logseq/getNameFromPage.ts
@@ -3,6 +3,8 @@ import {PageEntity} from "@logseq/libs/dist/LSPlugin";
 export default function getNameFromPage(page: PageEntity): string {
     return (
         page?.originalName ||
+        (page?.fullTitle as string | null) ||
+        page?.title ||
         page?.name ||
         null
     );
