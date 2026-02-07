23d3fa37ed3d2b8c5fc0b1a4a803b6139162599d
debanjandhar12 <debanjandhar12@gmail.com>
2026-02-07 14:36:59 +0530

    minor perf boost in build

--- vite.config.ts
+++ vite.config.ts
@@ -12,62 +12,64 @@ import {build} from "esbuild";
 function staticFileSyncTransformPlugin() {
     return {
         name: "staticFileSyncTransformPlugin",
+        enforce: "pre" as const,
         transform(code, id) {
-            if (/\.(js|ts|tsx|jsx|mjs)(\?.*)?$/.test(id) && code.includes("readFileSync")) {
-                let curDir = path.dirname(id);
-                if (curDir.includes("node_modules/.vite")) {
-                    // We are in a vite cache folder. We need original path.
-                    curDir = path.join(
-                        __dirname,
-                        "/node_modules/",
-                        path.parse(path.basename(id)).name,
-                    );
-                }
-                const ast = parseSync(code, {sourceType: "module"});
-                traverse(ast, {
-                    Identifier(nodePath) {
-                        if (nodePath.node.name === "__dirname") {
-                            nodePath.replaceWithSourceString(JSON.stringify(curDir));
-                        }
-                    },
-                });
-                traverse(ast, {
-                    CallExpression(nodePath) {
-                        const {callee, arguments: args} = nodePath.node;
-                        if (
-                            callee.type === "MemberExpression" &&
-                            callee.object.name === "path" &&
-                            callee.property.name === "join"
-                        ) {
-                            nodePath.replaceWithSourceString(
-                                JSON.stringify(path.join(...args.map((arg) => arg.value))),
-                            );
-                        }
-                    },
-                });
-                traverse(ast, {
-                    CallExpression(nodePath) {
-                        const {callee, arguments: args} = nodePath.node;
-                        if (
-                            callee.type === "MemberExpression" &&
-                            callee.object.name === "fs" &&
-                            callee.property.name === "readFileSync"
-                        ) {
-                            const filePath = args[0].value;
-                            try {
-                                const fileContents = fs.readFileSync(filePath, "utf-8");
-                                nodePath.replaceWithSourceString(JSON.stringify(fileContents));
-                            } catch (e) {
-                                console.error(e);
-                            }
-                        }
-                    },
-                });
-                const generated = generate(ast, {retainLines: true});
-                code = generated.code;
-                const map = generated.map;
-                return {code, map};
+            if (!/\.(js|ts|tsx|jsx|mjs)(\?.*)?$/.test(id) || !code.includes("readFileSync")) {
+                return null;
             }
+            let curDir = path.dirname(id);
+            if (curDir.includes("node_modules/.vite")) {
+                // We are in a vite cache folder. We need original path.
+                curDir = path.join(
+                    __dirname,
+                    "/node_modules/",
+                    path.parse(path.basename(id)).name,
+                );
+            }
+            const ast = parseSync(code, {sourceType: "module"});
+            traverse(ast, {
+                Identifier(nodePath) {
+                    if (nodePath.node.name === "__dirname") {
+                        nodePath.replaceWithSourceString(JSON.stringify(curDir));
+                    }
+                },
+            });
+            traverse(ast, {
+                CallExpression(nodePath) {
+                    const {callee, arguments: args} = nodePath.node;
+                    if (
+                        callee.type === "MemberExpression" &&
+                        callee.object.name === "path" &&
+                        callee.property.name === "join"
+                    ) {
+                        nodePath.replaceWithSourceString(
+                            JSON.stringify(path.join(...args.map((arg) => arg.value))),
+                        );
+                    }
+                },
+            });
+            traverse(ast, {
+                CallExpression(nodePath) {
+                    const {callee, arguments: args} = nodePath.node;
+                    if (
+                        callee.type === "MemberExpression" &&
+                        callee.object.name === "fs" &&
+                        callee.property.name === "readFileSync"
+                    ) {
+                        const filePath = args[0].value;
+                        try {
+                            const fileContents = fs.readFileSync(filePath, "utf-8");
+                            nodePath.replaceWithSourceString(JSON.stringify(fileContents));
+                        } catch (e) {
+                            console.error(e);
+                        }
+                    }
+                },
+            });
+            const generated = generate(ast, {retainLines: true});
+            code = generated.code;
+            const map = generated.map;
+            return {code, map};
         },
     };
 }
@@ -77,7 +79,7 @@ function bundleJSStringPlugin(mode: string) {
         name: "bundleJSStringPlugin",
         async transform(code, id) {
             if (id.endsWith(".js?string")) {
-                const isProd = mode === 'production';
+                const isProd = mode === "production";
                 const testLogLevel = process.env.VITE_TEST_LOG_LEVEL;
                 const result = await build({
                     stdin: {
@@ -92,13 +94,15 @@ function bundleJSStringPlugin(mode: string) {
                     platform: "browser",
                     write: false,
                     loader: {
-                        '.css': 'empty', // Ignore CSS imports in bundled JS strings
+                        ".css": "empty", // Ignore CSS imports in bundled JS strings
                     },
                     define: {
-                        'import.meta.env.PROD': JSON.stringify(isProd),
-                        'import.meta.env.MODE': JSON.stringify(mode),
-                        'import.meta.env.VITEST': JSON.stringify(mode === 'test'),
-                        'import.meta.env.VITE_TEST_LOG_LEVEL': testLogLevel ? JSON.stringify(testLogLevel) : 'undefined',
+                        "import.meta.env.PROD": JSON.stringify(isProd),
+                        "import.meta.env.MODE": JSON.stringify(mode),
+                        "import.meta.env.VITEST": JSON.stringify(mode === "test"),
+                        "import.meta.env.VITE_TEST_LOG_LEVEL": testLogLevel
+                            ? JSON.stringify(testLogLevel)
+                            : "undefined",
                     },
                 });
                 return {
@@ -110,54 +114,56 @@ function bundleJSStringPlugin(mode: string) {
     };
 }
 
-export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({command, mode}) => {
+    const env = loadEnv(mode, process.cwd(), "");
     return {
         base: "./",
+        cacheDir: ".vite_cache",
         plugins: [
-            mode === 'development' && logseqDevPlugin(), // for dev only
-            mode === 'development' && reactPlugin(), // for dev only
+            mode === "development" && logseqDevPlugin(), // for dev only
+            mode === "development" && reactPlugin(), // for dev only
             nodePolyfills(),
             staticFileSyncTransformPlugin(),
             bundleJSStringPlugin(mode),
         ],
         define: {
-            'process.env': JSON.stringify({...env, NODE_ENV: mode}),
+            "process.env": JSON.stringify({...env, NODE_ENV: mode}),
         },
         server: {
             port: 5173,
             cors: true,
+            watch: {
+                ignored: ["**/dist/**", "**/node_modules/**"],
+            },
         },
         build: {
             sourcemap: true,
             target: "modules",
             minify: "esbuild",
+            emptyOutDir: true,
         },
         css: {
             postcss: {
-                plugins: [
-                    require('tailwindcss'),
-                    require('autoprefixer'),
-                ],
+                plugins: [require("tailwindcss"), require("autoprefixer")],
             },
         },
         test: {
-            include: ['**/*.test.ts'],
-            exclude: ['**/logseq-dev-plugin/**', '**/node_modules/**'],
-            setupFiles: ['./tests/setup.ts'],
+            include: ["**/*.test.ts"],
+            exclude: ["**/logseq-dev-plugin/**", "**/node_modules/**"],
+            setupFiles: ["./tests/setup.ts"],
             // todo: why alias?
             alias: {
-                '@logseq/libs': 'sass',
+                "@logseq/libs": "sass",
             },
-            environment: 'jsdom',
+            environment: "jsdom",
             env: {...env, NODE_ENV: mode},
-            pool: 'forks',
+            pool: "forks",
             poolOptions: {
                 forks: {
                     singleFork: true,
                 },
             },
             fileParallelism: false,
-        }
+        },
     };
 });
