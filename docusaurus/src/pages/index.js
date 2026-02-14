13dc585675bc1aa451bc7f36ed23603b63fa4ed5
debanjandhar12 <debanjandhar12@gmail.com>
2026-02-14 12:44:28 +0530

    fix: docusaurus loading 404 error

--- docusaurus/src/pages/index.js
+++ docusaurus/src/pages/index.js
@@ -1,12 +1,18 @@
-import React, { useEffect } from 'react';
-import { useHistory } from '@docusaurus/router';
+import React, {useEffect} from "react";
+import Head from "@docusaurus/Head";
+import {useHistory} from "@docusaurus/router";
 
 export default function Home() {
-  const history = useHistory();
-  
-  useEffect(() => {
-    history.push('./docs/intro/');
-  }, [history]);
-  
-  return null;
+    const history = useHistory();
+
+    useEffect(() => {
+        history.replace("./docs/intro");
+    }, [history]);
+
+    return (
+        <Head>
+            <meta httpEquiv="refresh" content="0; url=./docs/intro" />
+            <link rel="canonical" href="./docs/intro" />
+        </Head>
+    );
 }
